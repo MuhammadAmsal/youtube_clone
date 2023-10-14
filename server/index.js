a459729ca364e7f617bcf2d6316e2e5863185135
Muhammad Amsal <muhammadamsal526@gmail.com>
2023-10-15 00:21:51 +0500

    backend connected

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const app=express()
+dotenv.config()
+
+const connect=()=>{
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("connected to DB");
+    }).catch((err)=>{
+        throw err
+    })
+}
+
+
+
+
+
+app.listen(3000,()=>{
+    connect()
+    console.log("Connected!");
+})
